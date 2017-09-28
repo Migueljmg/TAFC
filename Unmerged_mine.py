939c5a44c4150079bc35edbaad15dd0ea0407df2
Miguel Gonçalves <miguel_goncalves_sg@hotmail.com>
2017-09-28 14:17:42 +0100

    a

--- Unmerged_mine.py
+++ Unmerged_mine.py
@@ -1,26 +1,40 @@
 import math
+import matplotlib.pyplot as plt
 
 class Problem:
-    def __init__(self, number,initial_positions, initial_velocities,deltat,plasma_freq,delta1):
-        self.init_pos=sorted(initial_positions)
-        self.init_vel=[x for _,x in sorted(zip(initial_velocities,initial_positions]
+    def __init__(self, number,initial_positions, initial_velocities,wpdt,delta1,n_iter):
+        self.init_pos=sorted(initial_positions) #in units of Debye length
+        #self.init_vel=[x for _,x in sorted(zip(initial_velocities,initial_positions))] #in units of thermal velocity #acho que isto estava ao contrario
+        self.init_vel=[x for _,x in sorted(zip(initial_positions,initial_velocities))] #in units of thermal velocity
         self.N=number #number of particles
-        self.pos=[[]] #final matrix of positions 
-        self.vel=[[]] #final matrix of velocities
-        self.omegap=plasma_freq
-        self.delta=delta1
-        self.eq_pos=[self.delta*n for n in range(N)]
+        self.num_iter=n_iter #number of iterations
+        self.pos=[[0] * self.N for i in range(self.num_iter)] #final matrix of positions 
+        self.vel=[[0] * self.N for i in range(self.num_iter)] #final matrix of velocities
+        self.a=wpdt #adimensional measure of time
+        self.delta=delta1 #in units of the Debye length
+        self.eq_pos=[self.delta*n for n in range(self.N)]
 
-    def start(self, num_iter,deltat):
+    def start(self):
         "num_iter -> number of iterations; deltat-> iteration time"
         self.pos[0]=self.init_pos
         self.vel[0]=self.init_vel
         
-        argument=self.omegap*deltat
+        argument=self.a
         cos=math.cos(argument)
         sin=math.sin(argument)
 
-        for k in range(1,num_iter):
-            for i in range(N):
-                self.vel[k][i]=self.vel[k-1,i]*cos-self.omegap*sin*(self.pos[k-1][i]-self.eq_pos[i])
-                self.pos[k][i]=self.pos[k-1][i]+self.vel[k-1,i]*sin-(self.pos[k-1][i]-self.eq_pos[i])*(1-cos)
+        print(self.init_vel) #VER main. Isto e so para verificar, podes apagar dps
+
+        for k in range(1,self.num_iter):
+            for i in range(self.N):
+                self.vel[k][i]=self.vel[k-1][i]*cos-sin*(self.pos[k-1][i]-self.eq_pos[i])
+                self.pos[k][i]=self.pos[k-1][i]+self.vel[k-1][i]*sin-(self.pos[k-1][i]-self.eq_pos[i])*(1-cos)
+
+        t_list = [self.a*i for i in range(self.num_iter)]
+        vel_list=[self.vel[k][3] for k in range(self.num_iter)]
+        pos_list=[self.pos[k][3] for k in range(self.num_iter)]
+        plt.plot(t_list,pos_list)#I'm printing particle's 3 positions as an example
+        plt.show()
+            
+                
+                    
--- main.py
+++ main.py
@@ -1,3 +1,22 @@
-from Unmerged import Problem
+from Unmerged_mine import Problem
+import random
+import numpy as np
+import matplotlib.pyplot as plt
 
-#p1 = Problem(1,1,1,1,1,1)
+#General way of creating 2D arrays
+#a = [[0] * ncol for i in range(nrow)]
+
+N=10
+num_iter=200
+delta=0.1 #1/10 of the Debye length
+init_pos=[delta*i for i in range(N)]
+init_vel=[random.uniform(-0.01,0.01) for i in range(N)]#velocities such that there are no crossings to test
+#init_vel=[0.01 for i in range(N)]
+wpdt=0.05
+
+print(init_vel) #para verificar que da igual ao que entra na classe (initial_vels). Aquilo do zip como estava antes fazia com que o self.init_vel recebesse posicoes em vez de vels. Troquei e parece tar bem agora. Podes apagar isto dps.
+
+p1 = Problem(N,init_pos,init_vel,wpdt,delta,num_iter)
+
+p1.start()
+

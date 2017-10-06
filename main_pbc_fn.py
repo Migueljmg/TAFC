322812efa090293051a1e929ac1a121b1aff233f
Miguel Gonçalves <miguel_goncalves_sg@hotmail.com>
2017-10-06 13:33:15 +0100

    a

--- main_pbc_fn.py
+++ main_pbc_fn.py
@@ -1,65 +1,150 @@
 from solver_pbc_fn import Problem
+from scipy.optimize import curve_fit
 import random
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import os
+from time import time
+
+
+def plot_energy(period):
+    b=p1.getallenergy(period)
+    plt.plot(range(len(b)),b)
+    plt.grid(True)
+    plt.show()
+
+def plot_vel_distr(num_iter,period):
+    vlist=p1.getvelocities(num_iter-1-(num_iter-1)%period)
+    n,bins,patches=plt.hist(vlist, bins=20)
+    bin_centers=bins[:-1]+0.5*(bins[1:]-bins[:-1])
+
+    def func(x, maximum_y, x0, sigma):
+        return maximum_y*np.exp(-(x-x0)**2/(2*sigma**2))
+
+    from scipy.optimize import curve_fit
+    # Edit the starting values and the function to adapt for your need
+    popt, pcov = curve_fit(func, bin_centers, n, p0 = [10, 0, 2])
+
+    plt.plot(bin_centers, func(bin_centers, *popt), 'r-')
+    plt.grid(True)
+    plt.show()
+
+def animate(num_iter,period):
+    traj=p1.getalltrajectories(period)
+
+    sheet_figure = plt.figure()
+    ax = plt.axes(xlim=(-delta, N*delta), ylim=(-0.1, 0.1))
+    point_set, = ax.plot([traj[0][i] for i in range(N)],[0 for i in range(N)], 'ro')
+    plt.axvline(x=-delta/2)
+    plt.axvline(x=(N-0.5)*delta)
+    pt_circ = plt.Circle((4, 4), 1, color='b', fill=False)
+    ax.add_artist(pt_circ)
+
+    def run_animation(i):
+        point_set.set_data([traj[i][k] for k in range(N)],[0 for k in range(N)])
+        return point_set,
+
+    anim = animation.FuncAnimation(sheet_figure, run_animation, frames=(num_iter-1)/period, interval=10)
+
+    plt.show()
 
 #General way of creating 2D arrays
 #a = [[0] * ncol for i in range(nrow)]
-
+os.system("rm ./lixo/*.pkl")
 random.seed(42)
-
-N=10#4
+t0=time()
+N=10
 wpdt=0.001
-num_iter=5000
-delta=0.1 #1/10 of the Debye length
-nr_images=0 #I'm sending this but it'll not matter, I have to remove it from the class
-init_pos=[delta*i for i in range(N)]
-init_vel=[1 for i in range(N)]
-#init_vel[0]=-3
-#init_vel[1]=-0.5
-
-
-p1 = Problem(N,init_pos,init_vel,wpdt,delta,num_iter,nr_images)
-
-p1.start()
-
-
-
-
-"""
-random.seed(42)
-
-N=50
-wpdt=0.05
-num_iter=20
+num_iter=10000
 delta=0.1 #1/10 of the Debye length
 nr_images=0 #number of image charges
+period=10
 init_pos=[delta*i for i in range(N)]
-init_vel=[random.uniform(-1,1) for i in range(N)]
-init_vel[0]=0.5
-init_vel[1]=-0.5
+init_vel=[random.uniform(-0.3,0.3) for i in range(N)]
+#init_vel[0]=3
+#init_vel[1]=-0.7
+#init_vel[2]=0.05
+#init_vel[3]=0.05
+p1 = Problem(N,init_pos,init_vel,wpdt,delta,num_iter,nr_images,period)
+
+p1.start()
+t1=time()
+
+print(t1-t0)
+#p1.cenas()
+
+#plot_energy(period)
+#plot_vel_distr(num_iter,period)
+animate(num_iter,period)
+
+
+"""
+#TRAJECTORY
+a=p1.getalltrajectories(period)
+print(len(a))
+plt.plot(range(num_iter-1), [c[0] for c in a])
+plt.plot(range(num_iter-1), [c[1] for c in a])
+plt.grid(True)
+plt.show()
 """
 
 
 """
-##########Examples of image sheets list handling
-#Velocities
-n=3
-for i in range(n):
-    init_vel.append(init_vel[i])
-
-for i in range(n):
-    init_vel.insert(0,init_vel[N-1])#don't need to put i as I am inserting elements at the left of the list
-
-print(init_vel)
-
-#Positions
-n=5
-for i in range(n):
-    init_pos.append(init_pos[i]+N*delta)
-
-for i in range(n):
-    init_pos.insert(0,init_pos[N-1]-N*delta)#don't need to put i as I am inserting elements at the left of the list
-
-print(init_pos)
+#ENERGY
+b=p1.getallenergy(period)
+plt.plot(range(len(b)),b)
+plt.grid(True)
+plt.show()
 """
+
+"""
+#MAXWELLIAN
+vlist=p1.getvelocities(num_iter-1-(num_iter-1)%period)
+n,bins,patches=plt.hist(vlist, bins=20)
+bin_centers=bins[:-1]+0.5*(bins[1:]-bins[:-1])
+
+def func(x, maximum_y, x0, sigma):
+    return maximum_y*np.exp(-(x-x0)**2/(2*sigma**2))
+
+from scipy.optimize import curve_fit
+# Edit the starting values and the function to adapt for your need
+popt, pcov = curve_fit(func, bin_centers, n, p0 = [10, 0, 2])
+
+plt.plot(bin_centers, func(bin_centers, *popt), 'r-')
+plt.grid(True)
+plt.show()
+"""
+
+"""
+#ANIMATION
+
+traj=p1.getalltrajectories(period)
+
+#for i in range(num_iter-1):
+#    print(i,traj[i][9])
+
+
+sheet_figure = plt.figure()
+ax = plt.axes(xlim=(-delta, N*delta), ylim=(-0.1, 0.1))
+point_set, = ax.plot([traj[0][i] for i in range(N)],[0 for i in range(N)], 'ro')
+plt.axvline(x=-delta/2)
+plt.axvline(x=(N-0.5)*delta)
+pt_circ = plt.Circle((4, 4), 1, color='b', fill=False)
+ax.add_artist(pt_circ)
+#ttl = ax.text(.5, 1.005, '', transform = ax.transAxes)
+
+def run_animation(i):
+    point_set.set_data([traj[i][k] for k in range(N)],[0 for k in range(N)])
+    #ttl.set_text(str(i))    
+    return point_set,
+
+anim = animation.FuncAnimation(sheet_figure, run_animation, frames=(num_iter-1)/period, interval=5)
+
+plt.show()
+"""
+
+
+
+
+os.system("rm ./lixo/*.pkl")
--- solver_pbc_fn.py
+++ solver_pbc_fn.py
@@ -1,13 +1,15 @@
 import math
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import scipy.stats as stats
+import pickle
 
 class Problem:
-    def __init__(self, number,initial_positions, initial_velocities,wpdt,delta1,n_iter,n_image):
+    def __init__(self, number,initial_positions, initial_velocities,wpdt,delta1,n_iter,n_image,period):
 
         self.a=wpdt #adimensional measure of time
         self.delta=delta1 #in units of the Debye length
-
+        self.period=period
         self.init_pos=sorted(initial_positions) #in units of Debye length
         self.init_vel=[x for _,x in sorted(zip(initial_positions,initial_velocities))] #in units of thermal velocity
         self.N=number #number of particles
@@ -18,172 +20,284 @@ class Problem:
         self.vec_n_i_left.append(0)#we only start calculating the energy for k=1
 
 
-        self.pos=[[0] * (self.N) for i in range(self.num_iter)] #final matrix of positions 
-        self.vel=[[0] * (self.N) for i in range(self.num_iter)] #final matrix of velocities
+        #self.pos=[[0] * (self.N) for i in range(self.num_iter)] #final matrix of positions 
+        #self.vel=[[0] * (self.N) for i in range(self.num_iter)] #final matrix of velocities
         self.eq_pos=[self.delta*n for n in range(self.N)]
         
-
 
         self.energy=[0 for n in range(n_iter)]
 
     def start(self):
         "num_iter -> number of iterations; deltat-> iteration time"
-        self.pos[0]=self.init_pos
-        self.vel[0]=self.init_vel
-
+        self.prepos=self.init_pos
+        self.prevel=self.init_vel
+        self.pospos=[0 for i in range(len(self.prepos))]
+        self.posvel=[0 for i in range(len(self.prepos))]
         
         argument=self.a
         a=self.a
         cos=math.cos(argument)
         sin=math.sin(argument)
-        pos=self.pos
 
+
+        img_count_left=0
+        img_count_right=0
+        img_count_max=0
             
         for k in range(1,self.num_iter):
+
+            for i in range(len(self.pospos)):
+                del self.pospos[0]
+
+            for i in range(len(self.posvel)):
+                del self.posvel[0]
+
+            self.pospos = [0 for i in range(self.N)]
+            self.posvel = [0 for i in range(self.N)]
+
+
+            #print(k)
+            left_i=0
+            right_i=0
+            preeq_pos=[self.eq_pos[i] for i in range(len(self.eq_pos))]
+            #print(self.prepos)
+
+
+            #print("\n begin ")
+            #print(k-1,self.prepos)
+
+            #if(k>141):
+
+            #Check which particles are inside the box 
+            for i in range(len(self.prepos)):
+                if(self.prepos[i]>-self.delta/2):
+                    left_i=i
+                    break
+            for i in range(len(self.prepos)):
+                if(self.prepos[i]>(self.N-0.5)*self.delta):
+                    right_i=i-1
+                    break
+                if(i==len(self.prepos)-1):#if nothing is on the right of the box
+                    right_i=len(self.prepos)-1
+
+            
+
+            #Clear the previous particles outside the box
+         
+            for i in range(left_i):
+                del self.prepos[0]
+                del self.prevel[0]
+                del self.eq_pos[0]
+
+
+            #After deleting the previous entries, right_i corresponds to N-1
+            for i in range(self.N,len(self.prepos)):
+                del self.prepos[self.N]
+                del self.prevel[self.N]
+                del self.eq_pos[self.N]
+            
+        
+            #print(k,self.eq_pos,left_i,right_i)
+            if (k-1)%self.period==0:
+                pickle.dump([self.prepos,self.eq_pos], open('./lixo/'+str(k-1)+'pos'+'.pkl', 'wb'))
+                pickle.dump(self.prevel, open('./lixo/'+str(k-1)+'vel'+'.pkl', 'wb'))
+
+
+            
+            kinetic=sum([vels*vels for vels in self.prevel])
+            diff=[self.prepos[i]-self.eq_pos[i] for i in range(len(self.prepos))]
+            potential=sum([diffs*diffs for diffs in diff])
+            self.energy[k-1]=kinetic+potential
+
 
             img_count_left=0
             img_count_right=0
             img_count_max=0
 
 
-            
-            #Clear the previous images
-            for i in range(self.n_i):
-                del self.pos[k-1][0]
-                del self.vel[k-1][0]
-                del self.eq_pos[0]
+            #print(self.prevel)
 
-
-            for i in range(self.N,self.N+self.n_i):
-                del self.pos[k-1][self.N]
-                del self.vel[k-1][self.N]
-                del self.eq_pos[self.N]
-
-            
 
             in1=True
             in2=True
             for i in range(self.N):
                     
-                self.vel[k][i]=self.vel[k-1][i]*cos-sin*(self.pos[k-1][i]-self.eq_pos[i])
-                self.pos[k][i]=self.pos[k-1][i]+self.vel[k-1][i]*sin-(self.pos[k-1][i]-self.eq_pos[i])*(1-cos)
+                self.posvel[i]=self.prevel[i]*cos-sin*(self.prepos[i]-self.eq_pos[i])
+                self.pospos[i]=self.prepos[i]+self.prevel[i]*sin-(self.prepos[i]-self.eq_pos[i])*(1-cos)
 
 
-                if(self.pos[k][i]>(self.N-0.5)*self.delta and in1==True):
+                if(self.pospos[i]>(self.N-0.5)*self.delta and in1==True):
                     img_count_left=self.N-i
                     in1=False
 
 
-                if(self.pos[k][self.N-i-1]<-0.5*self.delta and in2==True):
+                if(self.pospos[self.N-i-1]<-0.5*self.delta and in2==True):
                     img_count_right=self.N-i
                     in2=False
-
+                
+              #print(k,self.pospos,self.eq_pos)
 
             img_count_max=max(img_count_left,img_count_right)
 
 
-            self.vec_n_i_left.append(img_count_max)
-
             #Create new images
             self.n_i=img_count_max
             
-            #First creation of images
+
+            #print(self.prepos)
+
+            #print(k,self.pospos,self.eq_pos)
+
+            #Creation of images
             for i in range(self.n_i):
                 #Right images
-                self.vel[k].append(self.vel[k][i])
-                self.pos[k].append(self.pos[k][i]+self.N*self.delta)
+                self.posvel.append(self.posvel[i])
+                self.pospos.append(self.pospos[i]+self.N*self.delta)
                 self.eq_pos.append(self.eq_pos[i]+self.N*self.delta)
                 #We want to guarantee that we have the same number of images for k and k-1. This is needed for the computation of the crossing. So
-                self.vel[k-1].append(self.vel[k-1][i])
-                self.pos[k-1].append(self.pos[k-1][i]+self.N*self.delta)
-                
+                self.prevel.append(self.prevel[i])
+                self.prepos.append(self.prepos[i]+self.N*self.delta)
+
+            
+
             for i in range(self.n_i):
                 #Left images
-                self.vel[k].insert(0,self.vel[k][self.N-1])
-                self.pos[k].insert(0,self.pos[k][self.N-1]-self.N*self.delta)
-                self.vel[k-1].insert(0,self.vel[k-1][self.N-1])
-                self.pos[k-1].insert(0,self.pos[k-1][self.N-1]-self.N*self.delta)
+                self.posvel.insert(0,self.posvel[self.N-1])
+                self.pospos.insert(0,self.pospos[self.N-1]-self.N*self.delta)
+                self.prevel.insert(0,self.prevel[self.N-1])
+                self.prepos.insert(0,self.prepos[self.N-1]-self.N*self.delta)
                 self.eq_pos.insert(0,self.eq_pos[self.N-1]-self.N*self.delta)
 
+
+            #print(k,self.pospos,self.eq_pos)
+            #print(k,self.pospos)
 
 
             #Verifying if there were any crossings taking place
             for i in range(1,self.N+2*self.n_i):
 
-                if(self.pos[k][i-1]>self.pos[k][i]):
+                if(self.pospos[i-1]>self.pospos[i]):
 
                    #Computation of tc1
-                    tc1 = self.a*(self.pos[k-1][i]-self.pos[k-1][i-1])/(self.pos[k-1][i]-self.pos[k-1][i-1] + self.pos[k][i-1]-self.pos[k][i])
+                    tc1 = self.a*(self.prepos[i]-self.prepos[i-1])/(self.prepos[i]-self.prepos[i-1] + self.pospos[i-1]-self.pospos[i])
                     # Position at t + tc1
-                    pos_tc1_im1 = self.pos[k-1][i-1]+self.vel[k-1][i-1]*math.sin(tc1)-(self.pos[k-1][i-1]-self.eq_pos[i-1])*(1-math.cos(tc1))## i-1 position
-                    pos_tc1_i = self.pos[k-1][i]+self.vel[k-1][i]*math.sin(tc1)-(self.pos[k-1][i]-self.eq_pos[i])*(1-math.cos(tc1))## i position
+                    pos_tc1_im1 = self.prepos[i-1]+self.prevel[i-1]*math.sin(tc1)-(self.prepos[i-1]-self.eq_pos[i-1])*(1-math.cos(tc1))## i-1 position
+                    pos_tc1_i = self.prepos[i]+self.prevel[i]*math.sin(tc1)-(self.prepos[i]-self.eq_pos[i])*(1-math.cos(tc1))## i position
 
 
                     #Computation of tc2
-                    tc2 = tc1*(self.pos[k-1][i]-self.pos[k-1][i-1])/(self.pos[k-1][i]-self.pos[k-1][i-1] + pos_tc1_im1-pos_tc1_i)
-                    #tc2= -math.atan((self.pos[k-1][i]-self.pos[k-1][i-1])/(self.vel[k-1][i]-self.vel[k-1][i-1]))
+                    tc2 = tc1*(self.prepos[i]-self.prepos[i-1])/(self.prepos[i]-self.prepos[i-1] + pos_tc1_im1-pos_tc1_i)
+                    #tc2= -math.atan((self.prepos[i]-self.prepos[i-1])/(self.prevel[i]-self.prevel[i-1]))
 
 
                     # Positions at t + tc2
-                    pos_tc2_im1 = self.pos[k-1][i-1]+self.vel[k-1][i-1]*math.sin(tc2)-(self.pos[k-1][i-1]-self.eq_pos[i-1])*(1-math.cos(tc2))## i-1 position
-                    pos_tc2_i = self.pos[k-1][i]+self.vel[k-1][i]*math.sin(tc2)-(self.pos[k-1][i]-self.eq_pos[i])*(1-math.cos(tc2))## i position
+                    pos_tc2_im1 = self.prepos[i-1]+self.prevel[i-1]*math.sin(tc2)-(self.prepos[i-1]-self.eq_pos[i-1])*(1-math.cos(tc2))## i-1 position
+                    pos_tc2_i = self.prepos[i]+self.prevel[i]*math.sin(tc2)-(self.prepos[i]-self.eq_pos[i])*(1-math.cos(tc2))## i position
                     #velocities at t+tc2
-                    vel_tc2_im1=self.vel[k-1][i-1]*math.cos(tc2)-math.sin(tc2)*(self.pos[k-1][i-1]-self.eq_pos[i-1])## i-1 velocity
-                    vel_tc2_i=self.vel[k-1][i]*math.cos(tc2)-math.sin(tc2)*(self.pos[k-1][i]-self.eq_pos[i])## i velocity
+                    vel_tc2_im1=self.prevel[i-1]*math.cos(tc2)-math.sin(tc2)*(self.prepos[i-1]-self.eq_pos[i-1])## i-1 velocity
+                    vel_tc2_i=self.prevel[i]*math.cos(tc2)-math.sin(tc2)*(self.prepos[i]-self.eq_pos[i])## i velocity
 
 
 
                     # Evolution from tc2 to t+dt
                     #Positions
-                    self.pos[k][i-1]=pos_tc2_im1+vel_tc2_im1*math.sin(self.a-tc2)-(pos_tc2_im1-self.eq_pos[i-1])*(1-math.cos(self.a-tc2))+(self.a-tc2)*(self.a-tc2)/2*self.delta
-                    self.pos[k][i]=pos_tc2_i+vel_tc2_i*math.sin(self.a-tc2)-(pos_tc2_i-self.eq_pos[i])*(1-math.cos(self.a-tc2))-(self.a-tc2)*(self.a-tc2)/2*self.delta
+                    self.pospos[i-1]=pos_tc2_im1+vel_tc2_im1*math.sin(self.a-tc2)-(pos_tc2_im1-self.eq_pos[i-1])*(1-math.cos(self.a-tc2))+(self.a-tc2)*(self.a-tc2)/2*self.delta
+                    self.pospos[i]=pos_tc2_i+vel_tc2_i*math.sin(self.a-tc2)-(pos_tc2_i-self.eq_pos[i])*(1-math.cos(self.a-tc2))-(self.a-tc2)*(self.a-tc2)/2*self.delta
                     #Velocities
-                    self.vel[k][i-1]=vel_tc2_im1*math.cos(self.a-tc2)-math.sin(self.a-tc2)*(pos_tc2_im1-self.eq_pos[i-1])+(self.a-tc2)*self.delta
-                    self.vel[k][i]=vel_tc2_i*math.cos(self.a-tc2)-math.sin(self.a-tc2)*(pos_tc2_i-self.eq_pos[i])-(self.a-tc2)*self.delta
+                    self.posvel[i-1]=vel_tc2_im1*math.cos(self.a-tc2)-math.sin(self.a-tc2)*(pos_tc2_im1-self.eq_pos[i-1])+(self.a-tc2)*self.delta
+                    self.posvel[i]=vel_tc2_i*math.cos(self.a-tc2)-math.sin(self.a-tc2)*(pos_tc2_i-self.eq_pos[i])-(self.a-tc2)*self.delta
 
 
             #Order all the particles according to their positions after each time interval dt
-            self.vel[k]=[x for _,x in sorted(zip(self.pos[k],self.vel[k]))]
-            self.pos[k].sort()
+            self.posvel=[x for _,x in sorted(zip(self.pospos,self.posvel))]
+            self.pospos.sort()
 
-
-            self.energy[k]=self.energy_from_velocity(self.particle_vel(k))+self.energy_from_position(self.particle_pos(k), self.particle_eq_pos(k))
 
             
-        #print(col_counter/self.num_iter)
+            #Clear the auxiliary k-1 entries used to calculate crossings
+            for i in range(img_count_max):
+                del self.prepos[0]
+                del self.prevel[0]
+
+            for i in range(self.N,self.N+img_count_max):
+                del self.prepos[self.N]
+                del self.prevel[self.N]
+
+
+            self.prevel=[self.posvel[i] for i in range(len(self.posvel))]
+            self.prepos=[self.pospos[i] for i in range(len(self.pospos))]
+
+
+
+    
+    def energy_from_velocity(self,vel):
+        return sum([vels*vels for vels in vel])
+
+    def energy_from_position(self,pos,pos_eq):
+        diff=[pos[i]-pos_eq[i] for i in range(len(pos))]
+        return sum([diffs*diffs for diffs in diff])
+
+
+
+    def gettrajectory(self,particle,period):
+        lm=0
+        rm=self.num_iter-1
+        t_list=[i for i in range(lm ,rm) if i%period==0]
+        traj=[]
+        for i in range(lm,rm):
+            if i%period==0:
+                pkl_file=open('./lixo/'+str(i)+'pos'+'.pkl','rb')
+                data=pickle.load(pkl_file)
+                traj.append(data[0][particle])
+                pkl_file.close()
+        return traj
+        
+    def getalltrajectories(self,period):
+        lm=0
+        rm=self.num_iter-1
+        traj=[]
+        t_list=[i for i in range(lm,rm) if i%period==0]
+        for i in range(lm,rm):
+            if i%period==0:
+                pkl_file=open('./lixo/'+str(i)+'pos'+'.pkl','rb')
+                data=pickle.load(pkl_file)
+                traj.append(data[0])
+                pkl_file.close()
+        return traj
+
+    def getallenergy(self,period):
+        lm=0
+        rm=self.num_iter-1
+        energy=[]
+        t_list=[i for i in range(lm, rm) if i%period==0]
+        for i in range(lm,rm):
+            if i%period==0:
+                pkl_file_pos=open('./lixo/'+str(i)+'pos'+'.pkl','rb')
+                pkl_file_vel=open('./lixo/'+str(i)+'vel'+'.pkl','rb')
+                data_pos=pickle.load(pkl_file_pos)
+                data_vel=pickle.load(pkl_file_vel)
+                energy_vel=self.energy_from_velocity(data_vel)
+                energy_pos=self.energy_from_position(data_pos[0],data_pos[1])
+                energy.append(energy_vel+energy_pos)
+                pkl_file_pos.close()
+                pkl_file_vel.close()
+
+        return energy
+
+    def getvelocities(self,k):
+        pkl_file=open('./lixo/'+str(k)+'vel.pkl','rb')
+        dat=pickle.load(pkl_file)
+        data=[dat[0][i] for i in range(len(dat))]
+        pkl_file.close()
+        return data
+
  
 
 
+    def cenas(self):        
         #energy conservation?
-        #print(self.particle_pos(4),self.eq_pos)
-        """
-        lm=0
-        rm=self.num_iter
-        t_list=[i for i in range(lm ,rm)]
-        pos_list1=[self.pos[i][0] for i in range(lm ,rm)]
-        pos_list2=[self.pos[i][1] for i in range(lm ,rm)]
-        pos_list3=[self.pos[i][2] for i in range(lm ,rm)]
-        pos_list4=[self.pos[i][3] for i in range(lm ,rm)]
-        pos_list5=[self.pos[i][4] for i in range(lm ,rm)]
-        pos_list6=[self.pos[i][5] for i in range(lm ,rm)]
-        pos_list7=[self.pos[i][6] for i in range(lm ,rm)]
-        pos_list8=[self.pos[i][7] for i in range(lm ,rm)]
-        """
-        lm=0
-        rm=self.num_iter
-        t_list=[i for i in range(lm ,rm)]
-        #pos_list=[[self.pos[i][l] for i in range(lm,rm)] for l in range(self.N)]
-        #for i in range(self.N):
-        #    plt.plot(t_list,pos_list[i])
-
-
-        #plt.axhline(y=self.delta*(self.N-0.5),xmin=0,xmax=self.num_iter,c="black")
-        #plt.axhline(y=-self.delta/2,xmin=0,xmax=self.num_iter,c="black")
-        #plt.plot(t_list,pos_list1,t_list,pos_list2,t_list,pos_list3,t_list,pos_list4,t_list,pos_list5,t_list,pos_list6,t_list,pos_list7,t_list,pos_list8,t_list,self.energy)
-        
-
-
-        self.energy[0]=self.energy[1]
+        print("oi")
+        self.energy[self.num_iter-1]=self.energy[self.num_iter-2]
         plt.plot(self.energy)
 
         plt.xlabel('iters)')
@@ -192,10 +306,11 @@ class Problem:
         plt.grid(True)
         #plt.savefig("test.png")
         plt.show()
-
+        
+"""
         #Histogram of the velocity distribution
-        vlist=[self.vel[self.num_iter-1][i] for i in range(self.n_i,self.n_i+self.N)]
-        plt.hist(vlist,normed=False,bins=20)#,range=[-0.95,0.95])
+        #vlist=[self.vel[self.num_iter-1][i] for i in range(self.N)]
+        #plt.hist(vlist,normed=False,bins=10)#,range=[-0.95,0.95])
 
         #plt.show()
         
@@ -218,7 +333,7 @@ class Problem:
             ttl.set_text(str(i))    
             return point_set,
 
-        anim = animation.FuncAnimation(sheet_figure, run_animation, frames=self.num_iter, interval=50)
+        anim = animation.FuncAnimation(sheet_figure, run_animation, frames=self.num_iter, interval=20)
 
 
         #plt.show()
@@ -239,34 +354,34 @@ class Problem:
     def particle_pos(self,k):
         pos_vec=[]
         num=0
-        for i in range(len(self.pos[k])):
-            if(-self.delta/2<self.pos[k][i]<self.delta*(self.N-0.5)):
-                pos_vec.append(self.pos[k][i])
+        for i in range(len(self.pospos)):
+            if(-self.delta/2<self.pospos[i]<self.delta*(self.N-0.5)):
+                pos_vec.append(self.pospos[i])
                 num+=1
 
-            #print(k,self.pos[k],pos_vec,num)
+            #print(k,self.pospos,pos_vec,num)
 
         return pos_vec
 
     def particle_vel(self,k):
         vel_vec=[]
-        for i in range(len(self.pos[k])):
-            if(-self.delta/2<self.pos[k][i]<self.delta*(self.N-0.5)):
-                vel_vec.append(self.vel[k][i])
+        for i in range(len(self.pospos)):
+            if(-self.delta/2<self.pospos[i]<self.delta*(self.N-0.5)):
+                vel_vec.append(self.posvel[i])
 
         return vel_vec
 
     
     def particle_eq_pos(self,k):
         equi_pos=[]
-        for i in range(len(self.pos[k])):
-            if(-self.delta/2<self.pos[k][i]<self.delta*(self.N-0.5)):
+        for i in range(len(self.pospos)):
+            if(-self.delta/2<self.pospos[i]<self.delta*(self.N-0.5)):
                 #I will have a different number of left images for every k
                 equi_pos.append((i-self.vec_n_i_left[k])*self.delta)
         
         return equi_pos
 
-   
+    """
                         
                         
                         
